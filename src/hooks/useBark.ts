a75f594f65635443a885f4a7bc775b57fa57bcd8
Nicolo' Fuccella <nico@soluzionifutura.it>
2023-07-07 16:31:28 +0200

    prevent audio buffering

--- src/hooks/useBark.ts
+++ src/hooks/useBark.ts
@@ -10,8 +10,12 @@ const useBark = (): UseBarkReturnType => {
     const { data: { url } } = await axios.post<AudioGenerationResponse>(`${import.meta.env.VITE_BARK_BASE_PATH as string}/audio/generation`, {
       prompt
     })
-    const audio = new Audio(`${import.meta.env.VITE_BARK_FILES_BASE_PATH as string}/files/${url}`)
-    await audio.play();
+    const response = await fetch(`${import.meta.env.VITE_BARK_FILES_BASE_PATH as string}/files/${url}`)
+    const arrayBuffer = await response.arrayBuffer()
+    const blob = new Blob([arrayBuffer])
+    const blobUrl = URL.createObjectURL(blob)
+    const audio = new Audio(blobUrl)
+    await audio.play()
     return audio
   }
 
