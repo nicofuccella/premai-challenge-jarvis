3d8af05d4e5255990d4b892bd55f607282fda2a2
tiero <3596602+tiero@users.noreply.github.com>
2023-10-07 19:36:28 +0200

    rename to tiero

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,21 @@
 import { FunctionComponent } from "react"
-import { Avatar, Flex, Link, Text } from "@chakra-ui/react"
+import { Avatar, Box, Flex, Link, Text } from "@chakra-ui/react"
 
 const Header: FunctionComponent = () => {
   return (
-    <Flex mb={4} alignItems={"center"} justifyContent={"center"}>
-      <Avatar
-        src={"./assets/ai.webp"}
-        mr={4}
-      />
-      <Text fontSize={"2xl"} align={"center"} wordBreak={"keep-all"} fontWeight={700}>
-        <Text as="span" color={"cyan.500"}>Jarvis</Text> powered by <Link color='teal.500' href='https://premai.io' target="_blank">
-          PremAI.io
-        </Link>
-      </Text>
-    </Flex>
+    <Box>
+      <Flex mb={4} alignItems={"center"} justifyContent={"center"}>
+        <Avatar
+          size={"2xl"}
+          src={"./assets/tiero.jpeg"}
+        />
+      </Flex>
+      <Flex mb={4} alignItems={"center"} justifyContent={"center"}>
+        <Text fontSize={"2xl"} align={"center"} wordBreak={"keep-all"} fontWeight={700}>
+          Tiero.AI
+        </Text>
+      </Flex>
+    </Box>
   )
 }
 export default Header
